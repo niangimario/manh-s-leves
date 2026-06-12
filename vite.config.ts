3780d35ee81acdf7449066026d3d3fd0857cd7bd
marioniangi <marioniangi07@gmail.com>
2026-06-12 23:10:04 +0100

    Clean up vite config - remove invalid nitro prerender

--- vite.config.ts
+++ vite.config.ts
@@ -12,11 +12,4 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
-  nitro: {
-    prerender: {
-      crawlLinks: false,
-      routes: ["/"],
-      ignore: ["/api"],
-    },
-  },
 });
