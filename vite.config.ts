95b0c4a0791fbdba98d2550ed70d648c156058c5
marioniangi <marioniangi07@gmail.com>
2026-06-12 22:55:48 +0100

    Revert Vercel attempts - will use Railway instead

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,11 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  nitro: {
+    prerender: {
+      crawlLinks: false,
+      routes: ["/"],
+      ignore: ["/api"],
+    },
+  },
 });
